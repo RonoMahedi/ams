43feeebc443d79a2eeba89bd0f222884151d6010
ronomahedi <kalamazad.cse@com>
2023-03-27 22:50:32 +0600

    address management system

---
+++ resources/views/layouts/backend/partials/scripts.blade.php
@@ -0,0 +1,25 @@
+  <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="/backend/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="/backend/assets/vendor/libs/popper/popper.js"></script>
+    <script src="/backend/assets/vendor/js/bootstrap.js"></script>
+    <script src="/backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="/backend/assets/vendor/libs/hammer/hammer.js"></script>
+
+    <script src="/backend/assets/vendor/libs/i18n/i18n.js"></script>
+    <script src="/backend/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+
+    <script src="/backend/assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="/backend/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="/backend/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="/backend/assets/js/dashboards-analytics.js"></script>
+
+    @stack('js')
